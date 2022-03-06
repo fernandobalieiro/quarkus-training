512784550e74fbd9a6e633256464e15d071f59cd
Fernando Matias Balieiro <fernandobalieiro@gmail.com>
2022-03-06 17:05:03 +0000

    Initial commit

---
+++ src/main/java/com/training/quarkus/service/CallLogService.java
@@ -0,0 +1,28 @@
+package com.training.quarkus.service;
+
+import com.training.quarkus.model.CallLog;
+import com.training.quarkus.model.Caller;
+
+public class CallLogService {
+
+    public static CallLogService instance = null;
+
+    // FIXME Missing field.
+
+    public CallLog getCallLogByPhone(final String phone) {
+        // FIXME Invoke repository method here.
+        return ;
+    }
+
+    public void printCallInformation(final Caller caller, final CallLog callLog) {
+        if (caller == null && callLog == null) {
+            System.out.println("Error: Call Information not found.\n");
+        } else {
+            System.out.println("Call Log Information:");
+            System.out.println("Caller: " + caller);
+            System.out.println("Call Log: " + callLog + "\n");
+        }
+    }
+
+    // TODO Implement missing getInstance Method
+}
---
+++ src/main/java/com/training/quarkus/service/CallerService.java
@@ -0,0 +1,17 @@
+package com.training.quarkus.service;
+
+import com.training.quarkus.model.Caller;
+
+public class CallerService {
+
+    private static CallerService instance = null;
+
+    // FIXME Missing dependency.
+
+    public Caller getCallerByPhone(final String phone) {
+        // FIXME Invoke repository method here.
+        return ;
+    }
+
+    // TODO Implement missing getInstance Method
+}
