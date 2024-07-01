30140d1d1c03b49b2b371e37e03d69f922d5ab6f
Fernando Balieiro <fernandobalieiro@gmail.com>
2024-07-01 12:14:59 +0100

    Upgraded Quarkus version.

--- src/main/java/com/training/quarkus/QuarkusTraining.java
+++ src/main/java/com/training/quarkus/QuarkusTraining.java
@@ -9,9 +9,10 @@ import java.util.Scanner;
 
 public class QuarkusTraining {
 
+    private static final CallerService callerService = CallerService.getInstance();
+    private static final CallLogService callLogService = CallLogService.getInstance();
+
     public static void main(String[] args) {
-        CallerService callerService = CallerService.getInstance();
-        CallLogService callLogService = CallLogService.getInstance();
 
         // A valid phone number to try out: +35199999991
 
@@ -21,7 +22,7 @@ public class QuarkusTraining {
             System.out.print("Enter phone Number: ");
             String phone = in.nextLine();
 
-            if (phone == null || phone.length() == 0) {
+            if (phone == null || phone.isEmpty()) {
                 System.out.println("Phone number cannot be blank.\n");
                 continue;
             }
--- src/main/java/com/training/quarkus/repository/CallerRepository.java
+++ src/main/java/com/training/quarkus/repository/CallerRepository.java
@@ -11,9 +11,9 @@ public class CallerRepository {
 
     private static CallerRepository instance = null;
 
-    private Map<String, Caller> callers = new HashMap<>();
+    private final Map<String, Caller> callers = new HashMap<>();
 
-    public CallerRepository() {
+    private CallerRepository() {
         createCallers();
     }
 
@@ -34,7 +34,7 @@ public class CallerRepository {
         );
     }
 
-    public static CallerRepository getInstance() {
+    public static synchronized CallerRepository getInstance() {
         if (instance == null) {
             instance = new CallerRepository();
         }
